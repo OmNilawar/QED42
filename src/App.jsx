0db8f9bcc8495f11878554bfb83a63b079635100
Omkar Nilawar <omkarnilawar9@gmail.com>
2024-12-29 15:42:36 +0530

    cart route added

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route pathbhvh="/cart" element={<Cart />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
     </>
   );
-}
+}   
 
 export default App
